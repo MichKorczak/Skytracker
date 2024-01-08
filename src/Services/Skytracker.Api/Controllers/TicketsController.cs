2c778f28b618651d9dc67b5b67b6861eaa93b403
SIIPOLSKA\mkorczak <michal.korczak.external@gmail.com>
2024-01-08 12:55:32 +0100

    Add project files.

---
+++ src/Services/Skytracker.Api/Controllers/TicketsController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Skytracker.Api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class TicketsController : ControllerBase
+    {
+        public async Task<IActionResult> Index()
+        {
+            await 
+        }
+    }
+}
