8ab2fb3e3bbdfb0add5aa35480842331c9d770e4
SIIPOLSKA\mkorczak <michal.korczak.external@gmail.com>
2024-01-17 15:50:57 +0100

    Added migration project.

---
+++ Skytracker.Db.Migrations/Options.cs
@@ -0,0 +1,9 @@
+﻿using CommandLine;
+
+namespace Skytracker.Db.Migrations;
+
+public class Options
+{
+    [Option('c', "connectionString", Required = false, HelpText = "Database connection string")]
+    public string ConnectionString { get; set; }
+}
--- src/Services/Skytracker.Api/Controllers/v1/FlightController.cs
+++ src/Services/Skytracker.Api/Controllers/v1/FlightController.cs
@@ -1,5 +1,6 @@
 ﻿using BuilderPart.Application.Mediator;
 using Microsoft.AspNetCore.Mvc;
+using Skytracker.Application.Queries;
 
 namespace Skytracker.Api.Controllers.v1
 {
@@ -15,10 +16,22 @@ namespace Skytracker.Api.Controllers.v1
             _bus = bus;
         }
 
-        [HttpGet]
-        public async Task<IActionResult> GetFlights()
+        [HttpGet("{departure}/{returnDate}/{from}/{to}/{numberOfTravelers}/{cabinClass}")]
+        public async Task<IActionResult> GetFlights(
+            DateTime departure, DateTime returnDate, string from, string to, short numberOfTravelers, short cabinClass)
         {
-            return Ok();
+            var query = new GetFlightsQuery {
+                 CabinClass = cabinClass,
+                 Departure = departure,
+                 Return = returnDate,
+                 From = from,
+                 To = to,
+                 NumberOfTravelers = numberOfTravelers                 
+            };
+
+            var result = await _bus.SendAsync(query);
+
+            return Ok(result);
         }
     }
 }
